caf257db7ddef88329023938b309d2541f80c260
tcuevhostor4-sudo <tcuevhostor4@gmail.com>
2026-08-01 21:57:20 +0200

    Update maim.lua

--- maim.lua
+++ maim.lua
@@ -8,9 +8,9 @@ local dlstatus = require('moonloader').download_status
 
 script_name('M-AIM')
 script_author('Pashenkov')
-script_version('1.0.5')
+script_version('1.0.9')
 
-local CURRENT_VERSION = '1.0.5'
+local CURRENT_VERSION = '1.0.9'
 local SCRIPT_URL = 'https://raw.githubusercontent.com/tcuevhostor4-sudo/Marsh/main/maim.lua'
 
 local cfgDir = getWorkingDirectory() .. '\\config'
